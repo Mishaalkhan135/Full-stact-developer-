1670f659355057f7b4377377074a2c7226531537
Mishaalkhan135 <80804969+Mishaalkhan135@users.noreply.github.com>
2021-05-09 11:01:02 -0700

    project3
    Script of project 3

---
+++ Project3/script.js
@@ -0,0 +1,49 @@
+//Get Dom Elements
+const container = document.querySelector('.container');
+const seats = document.querySelectorAll('.row.seat:not(.occupied)');
+const count = document.getElementById('count');
+const total = document.getElementById('total');
+const movieSelect = document.getElementById('movie');
+
+populateUI();
+let ticketPrice = +movieSelect.value;
+//Function for count of seats
+function updateSelectedCount(){
+    const selectedSeats = document.querySelectorAll('.row .seat.selected');
+    const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+    console.log(seatsIndex);
+    //length of seats
+    const selectedSeatsCount = selectedSeats.length;
+    //spam value
+    count.innerText = selectedSeatsCount;
+    total.innerText = selectedSeatsCount * ticketPrice;
+    localStorage.setItem('selectedSeats',JSON.stringify(seatsIndex));
+}
+//Save the movie data to local storage
+function setMovieData(movieIndex,moviePrice){
+    localStorage.setItem('selectMovieIndex',movieIndex);
+    localStorage.setItem('selectedMoviePrice');
+}
+//Get data from localstorage and populate UI
+function populateUI(){
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+    if(selectedSeats != null){
+        
+    }
+}
+//Event listner for click
+//1. Event Listner for contianer to check for click on seats
+container.addEventListener('click', e => {
+    if(e.target.classList.contains('seat') &&
+    !e.target.classList.contains('occupied')){
+        //select and deselect seats
+        e.target.classList.toggle('selected')
+        updateSelectedCount();
+    }
+})
+//2. Event Listner for movie select(Select moives)
+movieSelect.addEventListener('change', e =>{
+    ticketPrice = +e.target.value;
+    setMovieData(e.target.seatsIndex, e.target.value);
+    updateSelectedCount();
+})
