328ce0ee4b24695ee1e4c67091418720255b4b08
Mishaalkhan135 <80804969+Mishaalkhan135@users.noreply.github.com>
2021-05-15 12:55:53 -0700

    project 3 script
    project 3 script

--- Project3/script.js
+++ Project3/script.js
@@ -1,11 +1,12 @@
 //Get Dom Elements
 const container = document.querySelector('.container');
-const seats = document.querySelectorAll('.row.seat:not(.occupied)');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
 const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 
 populateUI();
+
 let ticketPrice = +movieSelect.value;
 //Function for count of seats
 function updateSelectedCount(){
@@ -21,16 +22,29 @@ function updateSelectedCount(){
 }
 //Save the movie data to local storage
 function setMovieData(movieIndex,moviePrice){
-    localStorage.setItem('selectMovieIndex',movieIndex);
-    localStorage.setItem('selectedMoviePrice');
+    localStorage.setItem('selectedMovieIndex',movieIndex);
+    localStorage.setItem('selectedMoviePrice',moviePrice);
 }
 //Get data from localstorage and populate UI
 function populateUI(){
     const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
     if(selectedSeats != null){
-        
+        movieSelect.selectedIndex = selectedMovieIndex;    
     }
 }
+//Get data from local storage and populate UI
+function populateUI(){
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+    if(selectedSeats != null && selectedSeats.length > 0){
+        seats.forEach((seat,index) => {
+            if(selectedSeats.indexOf(index) >- 1){
+                seat.classList.add('selected')
+            }
+        })
+    };
+    const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+}
+
 //Event listner for click
 //1. Event Listner for contianer to check for click on seats
 container.addEventListener('click', e => {
@@ -47,3 +61,6 @@ movieSelect.addEventListener('change', e =>{
     setMovieData(e.target.seatsIndex, e.target.value);
     updateSelectedCount();
 })
+
+//Intial count and total price
+updateSelectedCount();
