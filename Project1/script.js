4a4e2785e46aa5b142480cf7e396737646f6cd40
Mishaalkhan135 <80804969+Mishaalkhan135@users.noreply.github.com>
2021-08-02 18:15:08 +0500

    update script.js
    Added functionality to check email validation and lengths

--- Project1/script.js
+++ Project1/script.js
@@ -19,9 +19,29 @@ function showSuccess(input){
     formControl.className = 'form-control success';
 }
 //function to check if emmail is valid
-function isValidEmail(email){
+function checkEmail(input){
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+    if(re.test(input.value.trim())){
+        showSuccess(input);
+    } else{
+        showError(input,`Please provide a valid email`);
+    }
+}
+//function to check length
+function checkLength(input, min, max){
+    if( input.value.length < min){
+        showError(input,`${getFieldId(input)} needs to be atleast ${min} characters`);
+    }else if(input.value.length > max){
+        showError(input,`${getFieldId(input)} needs to be less than ${max} characters`);
+    }else{
+        showSuccess(input);
+    }
+}
+//function to check if password and confirm passwords match
+function checkPasswordsMatch(input1,input2){
+    if(input1.value !== input2.value){
+        showError(input2,"Passwords dont match");
+    }
 }
 //function to check if require feilds have data
 function checkRequired(inputArray){
@@ -44,4 +64,9 @@ form.addEventListener('submit',function(e){
     e.preventDefault();
     
     checkRequired([username,email,address,contact,password,password2]);
+    checkLength(username,3,15);
+    checkLength(password,6,30);
+    checkEmail(email);
+    checkPasswordsMatch(password,password2);
+
 })
